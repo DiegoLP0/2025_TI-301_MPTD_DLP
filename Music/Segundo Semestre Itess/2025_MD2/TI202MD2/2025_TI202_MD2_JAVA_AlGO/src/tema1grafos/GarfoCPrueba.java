9813dd8a8f35f9ac8f8afc35dafa68754562e5c2
leyvadiego016@gmail.com <josea@LAPTOP-ISRBPPM6>
2025-03-25 13:58:00 -0600

    Graph class Dijkstra passo 1, 2, 3

--- Music/Segundo Semestre Itess/2025_MD2/TI202MD2/2025_TI202_MD2_JAVA_AlGO/src/tema1grafos/GarfoCPrueba.java
+++ Music/Segundo Semestre Itess/2025_MD2/TI202MD2/2025_TI202_MD2_JAVA_AlGO/src/tema1grafos/GarfoCPrueba.java
@@ -7,39 +7,45 @@
 package tema1grafos;
 
 public class GarfoCPrueba {
-   public static void main (String [] args){
-       System.out.println("Crea un grafo sin vertices ni aristas:::::::::::");
-       GrafoC g1 = new GrafoC();
-       System.out.println(g1.toString());
-       
-       System.out.println("Crea un grafo con vertices y aristas:::::::::::");
-       int[]v ={1,2,3};
-       int[][]e ={{1,2},{2,3},{3,4}};
-       GrafoC g2 = new GrafoC(v,e);
-       System.out.println(g2.toString());
-       
-       System.out.println("Agregar vertices y aristas");
-       g1.agrgarVertice(1);
-       g1.agrgarVertice(2);
-       g1.agrgarArista(1,2);
-       System.out.println(g1.toString());
-       
-       System.out.println("Probar grado de un vertice ::::::::::::::::");
-       GrafoC g3 = new GrafoC();
-       g3.agrgarVertice(1);
-       g3.agrgarVertice(2);
-       g3.agrgarVertice(3);
-       
-       g3.agrgarArista(1, 1);
-       g3.agrgarArista(1, 2);
-       System.out.println("grado(1)=" + g3.grado(1));
-       System.out.println("grado(2)=" + g3.grado(2));
-       System.out.println("grado(3)=" + g3.grado(3));
-       System.out.println("grado(4)=" + g3.grado(4));
-       
-       
-       System.out.println("gradoTotal de g3 = " + g3.gradoTotal());
-       
-   } 
-   
+
+    public static void main(String[] args) {
+        System.out.println("Crea un grafo sin vertices ni aristas:::::::::::");
+        GrafoC g1 = new GrafoC();
+        System.out.println(g1.toString());
+
+        System.out.println("Crea un grafo con vertices y aristas:::::::::::");
+        int[] v = {1, 2, 3};
+        int[][] e = {{1, 2}, {2, 3}, {3, 4}};
+        GrafoC g2 = new GrafoC(v, e);
+        System.out.println(g2.toString());
+
+        System.out.println("Agregar vertices y aristas");
+        g1.agrgarVertice(1);
+        g1.agrgarVertice(2);
+        g1.agrgarArista(1, 2);
+        System.out.println(g1.toString());
+
+        System.out.println("Probar grado de un vertice ::::::::::::::::");
+        GrafoC g3 = new GrafoC();
+        g3.agrgarVertice(1);
+        g3.agrgarVertice(2);
+        g3.agrgarVertice(3);
+
+        g3.agrgarArista(1, 1);
+        g3.agrgarArista(1, 2);
+        System.out.println("grado(1)=" + g3.grado(1));
+        System.out.println("grado(2)=" + g3.grado(2));
+        System.out.println("grado(3)=" + g3.grado(3));
+        System.out.println("grado(4)=" + g3.grado(4));
+
+        System.out.println("gradoTotal de g3 = " + g3.gradoTotal());
+
+        System.out.print("Grafo ponderado======================");
+        int[] vG4 = {1, 2, 3, 4, 5, 6};
+        int[][] eG4 = {{1, 2}, {1, 3}, {2, 4}, {2, 5}, {3, 5}, {4, 5}, {4, 6}, {5, 6}};
+        int[] wG4 = {3, 4, 6, 5, 1, 2, 7, 12};
+        GrafoC g4 = new GrafoC(vG4, eG4, wG4);
+        System.out.println(g4.toString());
+        
+    }
 }
--- Music/Segundo Semestre Itess/2025_MD2/TI202MD2/2025_TI202_MD2_JAVA_AlGO/src/tema1grafos/GrafoC.java
+++ Music/Segundo Semestre Itess/2025_MD2/TI202MD2/2025_TI202_MD2_JAVA_AlGO/src/tema1grafos/GrafoC.java
@@ -7,27 +7,28 @@
 package tema1grafos;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class GrafoC {
-
+    
     private List<Integer> v;
     private List<List<Integer>> e;
     private List<Integer> w;
-
+    
     public GrafoC() {
         v = new ArrayList<>();
         e = new ArrayList<>();
         w = new ArrayList<>();
     }
-
+    
     private void initVE(int[] v, int[][] e) {
         this.v = new ArrayList<>();
         this.e = new ArrayList<>();
         for (int i = 0; i < v.length; i++) {
             this.v.add(v[i]);
         }
-
+        
         for (int i = 0; i < e.length; i++) {
             ArrayList<Integer> arista = new ArrayList<>();
             arista.add(e[i][0]);
@@ -35,11 +36,11 @@ public class GrafoC {
             this.e.add(arista);
         }
     }
-
+    
     public GrafoC(int[] v, int[][] e) {
         initVE(v, e);
     }
-
+    
     public GrafoC(int[] v, int[][] e, int[] w) {
         initVE(v, e);
         this.w = new ArrayList<>();
@@ -47,11 +48,19 @@ public class GrafoC {
             this.w.add(w[i]);
         }
     }
-
+    
     public void agrgarVertice(int v) {
         this.v.add(v);
     }
-
+    
+    public void agrgarArista(int u, int v) {
+        ArrayList<Integer> arista = new ArrayList<>();
+        arista.add(u);
+        arista.add(v);
+        this.e.add(arista);
+        
+    }
+    
     public void agrgarArista(int u, int v, int w) {
         ArrayList<Integer> arista = new ArrayList<>();
         arista.add(u);
@@ -59,7 +68,7 @@ public class GrafoC {
         this.e.add(arista);
         this.w.add(w);
     }
-
+    
     public int grado(int v) {
         /*
         boolean vValid = false;
@@ -73,7 +82,7 @@ public class GrafoC {
         if (!this.v.contains(v)) {
             return -1;
         }
-
+        
         int grado = 0;
         int gradototal;
         for (List<Integer> arista : this.e) {
@@ -87,7 +96,7 @@ public class GrafoC {
         }
         return grado;
     }
-
+    
     public int gradoTotal() {
         int gradoTotal = 0;
         for (Integer v : this.v) {
@@ -95,14 +104,46 @@ public class GrafoC {
         }
         return gradoTotal;
     }
-
-    public void dijkstra() {
-
+    
+    public void dijkstra(int verticeInicial, int verticeFinal) {
+        //INICIALIZAR
+        //CALCULAR INFINITO
+        int infinito = 0;
+        for (Integer w : this.w) {
+            infinito += w;
+        }
+        
+        infinito++;
+        System.out.println("Infinito: " + infinito);
+        //VERTICE FINAL A = 1
+        
+        Integer a = verticeInicial;
+        //VERTICE FINAL Z = 6
+        Integer z = verticeFinal;
+        //HACER EL PASO 1. Grafo T
+        GrafoC T = new GrafoC();
+        T.agrgarVertice(a);
+        System.out.println("T: " + T.toString());
+        //HACER EL PASO 2. L
+        // List
+        List<Integer> L = new ArrayList<>();
+        L.add(0); // 0 -> v1 1 -> v2 
+        for (int i = 1; i < this.v.size(); i++) {
+            L.add(infinito);
+        }
+        System.out.println("L " + L.toString());
+        //HACER EL PASO 3, F es un conjunto
+        Integer v = a;
+        HashSet<Integer> F = new HashSet();
+        F.add(a);
+        //ITERRAR PASO 4 
+        
+        //RESULTADO
     }
-
+    
     @Override
     public String toString() {
         return "GrafoC{" + "v=" + v.toString() + ", e=" + e.toString() + '}';
     }
-
+    
 }
